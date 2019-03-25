dc4dd6c42c549c154856d5de12985a581fe4a525
Serg-Ant <48640775+Serg-Ant@users.noreply.github.com>
2019-03-25 18:20:33 +0500

    Add files via upload
    Ura!

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from app import app
 from app import db
+from models import Measurement
+from datetime import datetime
 import view
 
 if __name__ == '__main__':
-    app.run()
+	a = Measurement()
+	b = datetime.now()
+	a.add_new_record('0123456789012345', 17.4, b)
+	db.session.add(a)
+	db.session.commit()
+	print("Success!")
+	app.run()
--- models.py
+++ models.py
@@ -1,9 +1,14 @@
 from app import db
 
-class Measurement(db.Model):
+class Measurement(db.Model, db.Session):
 	__tablename__ = 'measurement'
 
 	id = db.Column(db.Integer, primary_key=True)
 	id_dev = db.Column(db.String(16), nullable=False)
 	value = db.Column(db.Float, nullable=False)
 	time = db.Column(db.DateTime, nullable=False)
+
+	def add_new_record(self, id_dev, value, time):
+		self.id_dev = id_dev
+		self.value = value
+		self.time = time
