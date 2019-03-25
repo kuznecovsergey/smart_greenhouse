fc15498ca26343744ccde57762cc59e49092ffb9
moiseenko1 <48207882+moiseenko1@users.noreply.github.com>
2019-03-25 16:08:49 +0500

    Add files via upload

---
+++ mosquitto_sub.py
@@ -0,0 +1,31 @@
+import paho.mqtt.client as mqtt
+
+
+
+
+
+def on_connect(client, userdata, flags, rc):
+
+    print("Connected with result code "+str(rc))
+
+    client.subscribe("$SYS/#")
+
+
+
+
+
+def on_message(client, userdata, msg):
+
+    print(msg.topic+" "+str(msg.payload))
+
+
+
+client = mqtt.Client()
+
+client.on_connect = on_connect
+
+client.on_message = on_message
+
+client.connect(" ")
+
+client.loop_forever()
